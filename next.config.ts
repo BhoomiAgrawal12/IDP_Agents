6aec54112bbea41ae6589e466c0f38916ffa9881
kamini08 <kaminibanait03@gmail.com>
2025-04-08 08:48:59 +0530

    resolve conflicts

--- next.config.ts
+++ next.config.ts
@@ -9,12 +9,12 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline'",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'",
           },
         ],
       },
     ];
   },
 };
-
 export default nextConfig;
